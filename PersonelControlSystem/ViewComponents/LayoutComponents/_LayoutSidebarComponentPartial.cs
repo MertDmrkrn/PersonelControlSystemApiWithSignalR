e43c46d5b5d9321b3958dbc95cec5f8043f18e2f
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-02-01 00:01:42 +0300

    Added template in project and Created Partial Views with Component Partial

---
+++ PersonelControlSystem/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelControlSystem.ViewComponents.LayoutComponents
+{
+    public class _LayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
--- PersonelControlSystemApi/Program.cs
+++ PersonelControlSystemApi/Program.cs
@@ -26,7 +26,13 @@ builder.Services.AddScoped<IOhsReportService, OhsReportManager>();
 builder.Services.AddScoped<IOhsReportDal, EfOhsReportDal>();
 
 builder.Services.AddScoped<IPersonelService, PersonelManager>();
-builder.Services.AddScoped<IPersonelDal,EfPersonelDal>();
+builder.Services.AddScoped<IPersonelDal, EfPersonelDal>();
+
+builder.Services.AddScoped<IRequestService, RequestManager>();
+builder.Services.AddScoped<IRequestDal, EfRequestDal>();
+
+builder.Services.AddScoped<IShiftService, ShiftManager>();
+builder.Services.AddScoped<IShiftDal, EfShiftDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
