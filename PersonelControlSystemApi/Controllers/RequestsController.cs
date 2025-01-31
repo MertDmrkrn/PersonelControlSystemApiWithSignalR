ba8356a0619e406b8077c50dde277c34c2f1da2f
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-01-31 21:54:13 +0300

    Completed Api Controllers

---
+++ PersonelControlSystemApi/Controllers/RequestsController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonelControlSystemDto.RequestDto;
+
+namespace PersonelControlSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        private readonly IRequestService _requestService;
+        private readonly IMapper _mapper;
+
+        public RequestsController(IRequestService requestService, IMapper mapper)
+        {
+            _requestService = requestService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult RequestList()
+        {
+            var values = _requestService.TGetListAll();
+            return Ok(_mapper.Map<List<ResultRequestDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateRequest(CreateRequestDto createRequestDto)
+        {
+            var values = _mapper.Map<Request>(createRequestDto);
+            _requestService.TAdd(values);
+            return Ok("İstek Ekleme İşlemi Gerçekleştirildi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRequest(int id)
+        {
+            var values = _requestService.TGetByID(id);
+            _requestService.TDelete(values);
+            return Ok("İstek Silme İşlemi Gerçekleştirildi.");
+        }
+        [HttpPut]
+        public IActionResult UpdateRequest(UpdateRequestDto updateRequestDto)
+        {
+            var values = _mapper.Map<Request>(updateRequestDto);
+            _requestService.TUpdate(values);
+            return Ok("İstek Güncelleme İşlemi Gerçekleştirildi.");
+        }
+
+        [HttpGet("GetRequest")]
+        public IActionResult GetRequest(int id)
+        {
+            var values= _requestService.TGetByID(id);
+            return Ok(_mapper.Map<GetRequestDto>(values));
+        }
+    }
+}
---
+++ PersonelControlSystemApi/Controllers/ShiftsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonelControlSystemDto.ShiftDto;
+
+namespace PersonelControlSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShiftsController : ControllerBase
+    {
+        private readonly IShiftService _shiftService;
+        private readonly IMapper _mapper;
+
+        public ShiftsController(IShiftService shiftService, IMapper mapper)
+        {
+            _shiftService = shiftService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public IActionResult ShiftList()
+        {
+            var values = _shiftService.TGetListAll();
+            return Ok(_mapper.Map<List<ResultShiftDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateShift(CreateShiftDto createShiftDto)
+        {
+            var values = _mapper.Map<Shift>(createShiftDto);
+            _shiftService.TAdd(values);
+            return Ok("Vardiya Ekleme İşlemi Gerçekleştirildi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteShift(int id)
+        {
+            var values = _shiftService.TGetByID(id);
+            _shiftService.TDelete(values);
+            return Ok("Vardiya Silme İşlemi Gerçekleştirildi.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateShift(UpdateShiftDto updateShiftDto)
+        {
+            var values = _mapper.Map<Shift>(updateShiftDto);
+            _shiftService.TUpdate(values);
+            return Ok("Vardiya Güncelleme İşlemi Gerçekleştirildi.");
+        }
+
+        [HttpGet("GetShift")]
+        public IActionResult GetShift(int id)
+        {
+            var values= _shiftService.TGetByID(id);
+            return Ok(_mapper.Map<GetShiftDto>(values));
+        }
+    }
+}
