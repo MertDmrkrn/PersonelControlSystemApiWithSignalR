3c1d16b4c7e511b35dc69d8019aa00273bb227fc
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-02-18 23:38:02 +0300

    Added OhsReport Controller

---
+++ PersonelControlSystem/Controllers/OhsReportController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelControlSystem.Controllers
+{
+    public class OhsReportController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OhsReportController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
